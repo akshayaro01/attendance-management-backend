f81e2261612d3634eda972bb93c864d0163929b9
Akshay Arora <ankush.arora@edgenetworks.in>
2022-03-02 22:47:59 +0530

    OTP changes done

--- utilities/nodemailer.js
+++ utilities/nodemailer.js
@@ -7,19 +7,23 @@ const transport = nodemailer.createTransport({
     secure: true,
     debug: true,
     auth: {
-        user: process.env.GMAIL_USERNAME,
-        pass: process.env.GMAIL_PASSWORD
+        type: 'OAuth2',
+        user: 'pubgtrending@gmail.com',
+        clientId: '35955691910-apqkm8sh86qkrn31qp2br528jubero3f.apps.googleusercontent.com',
+        clientSecret: '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',
+        refreshToken: '1//04gTv7Wsy0rSCCgYIARAAGAQSNwF-L9IrCzSRD3Z4G53COuQ10pQ7OxDxG0uZiBsri0R-RzccCtlecXKQuxXhAGFo8JWiHmcGAVE',
+        accessToken: 'ya29.A0ARrdaM_RC9RR2dTygFEApWuKZlzs4M_cAmI8aqwK6VLH5a2nrsfVONhHPAeY3AsraA8BF1OLIwrDA1vFM_vdjgt-UePvk8QZvdsUstRxqFpoGnMNYHbC-ux9bNdevXHLDD1m86bbPlG7RX5kzF2g3oBM6hmw'
     }
 })
 
 const sendMail = async (email, secretToken, mode) => {
     try {
         if (mode == 'OTP') {
-            
+
             return await transport.sendMail({
-                from: process.env.GMAIL_USERNAME,
+                from: "noreply@akhsay.com",
                 to: email,
-                subject: "OTP Submission",
+                subject: "OTP to change password",
                 html: `
         <h1>Reset Password</h1>
         <p> Here is your otp to change the password ${secretToken} </p>
